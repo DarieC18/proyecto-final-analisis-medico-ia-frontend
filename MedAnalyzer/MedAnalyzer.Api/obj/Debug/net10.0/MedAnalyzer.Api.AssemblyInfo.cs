39a9f8c1d334332c3e2ca81da8ece808c91b80d3
AshleeRr <ashleeramirezrosario@gmail.com>
2026-07-28 17:51:04 -0600

    Agregar cambios de la rama QA a Ash-v1

--- MedAnalyzer/MedAnalyzer.Api/obj/Debug/net10.0/MedAnalyzer.Api.AssemblyInfo.cs
+++ MedAnalyzer/MedAnalyzer.Api/obj/Debug/net10.0/MedAnalyzer.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedAnalyzer.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b13f9ecd035bcdd122c9f23684ba5606f294a30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43efcd77b04faea46e808044c22ea16fcf5cc8f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedAnalyzer.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedAnalyzer.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
